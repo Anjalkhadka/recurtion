221a2be9b5b829c11c9c5b46cbef095c0258137b
anjalkhadka <Anjalkhadka>
2021-07-16 22:19:07 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+'''
+fibonacci series
+'''
+
+
+def fibonnaic(num):
+    if num <= 1:
+        return num
+    else:
+        return fibonnaic(num - 1) + fibonnaic(num - 2)
+
+
+num = int(input("enter a number : "))
+for i in range(num):
+    print(fibonnaic(i))
